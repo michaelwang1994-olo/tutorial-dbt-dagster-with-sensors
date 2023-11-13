71dbd9cd12138b29225b5a2e6c8b7399727770a2
Michael Wang <michael.wang@olo.com>
2023-11-13 10:06:25 -0600

    update README and sensors

--- jaffle_dagster/jaffle_dagster/sensors.py
+++ jaffle_dagster/jaffle_dagster/sensors.py
@@ -1,4 +1,4 @@
-from dagster import multi_asset_sensor, MultiAssetSensorEvaluationContext, EventLogEntry, AssetKey, AssetSelection, define_asset_job, RunRequest, SensorResult
+from dagster import asset_sensor, SensorEvaluationContext, EventLogEntry, AssetKey, AssetSelection, define_asset_job, RunRequest, SensorResult
 from .partitions import multi_partitions, static_partitions, dynamic_partitions
 
 
@@ -9,14 +9,14 @@ run_order_count_chart = define_asset_job(
 )
 
 
-@multi_asset_sensor(
-    monitored_assets=[AssetKey("customers")],
+@asset_sensor(
+    asset_key=AssetKey("customers"),
     job=run_order_count_chart,
     minimum_interval_seconds=60
 )
-def customer_sensor(context: MultiAssetSensorEvaluationContext):
-    asset_events = context.latest_materialization_records_by_key()
-    context.log.info(asset_events)
+def customer_sensor(context: SensorEvaluationContext, event: EventLogEntry):
+    context.log.info(event)
+    assert event.dagster_event and event.dagster_event.asset_key
     dynamic_partition_add_requests = []
     run_requests = []
     for dynamic_partition in ["1", "2", "3"]:
@@ -24,9 +24,9 @@ def customer_sensor(context: MultiAssetSensorEvaluationContext):
         for static_partition in static_partitions.get_partition_keys():
             run_requests.append(RunRequest(run_key=f"{dynamic_partition}|{static_partition}", partition_key=f"{dynamic_partition}|{static_partition}"))
         # run_requests.append(RunRequest(run_key=f"{static_partition}", partition_key=f"{static_partition}"))
-    context.advance_all_cursors()
+
     return SensorResult(
         run_requests=run_requests,
-        dynamic_partitions_requests=dynamic_partition_add_requests,
+        # dynamic_partitions_requests=dynamic_partition_add_requests,
         cursor=context.cursor
     )
